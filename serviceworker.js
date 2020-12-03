35ae26268bb038e4dbd3f52324dd1b43a2c5b2b4
diginoise <daniel.drozdzewski@gmail.com>
2020-12-03 17:24:41 +0000

    Adding slides and images to PWA to demonstrate slow load

--- serviceworker.js
+++ serviceworker.js
@@ -3,17 +3,27 @@ var staticCacheName = "pwa";
 self.addEventListener("install", function (e) { 
     e.waitUntil( 
         caches.open(staticCacheName).then(function (cache) { 
-        return cache.addAll(["/teaching-web-pwa/index.html"]); 
+            return cache.addAll([
+                "/index.html",
+                "/images/spacex1.jpg",
+                "/images/spacex2.jpg",
+                "/images/icon-128.png",
+                "/images/icon-512.png",
+                "/style.css"
+            ]); 
         }) 
     ); 
 }); 
 
-self.addEventListener("fetch", function (event) { 
-    console.log(event.request.url); 
-
-    event.respondWith( 
-        caches.match(event.request).then(function (response) { 
-        return response || fetch(event.request); 
-        }) 
-    ); 
-}); 
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.open(staticCacheName).then(function(cache) {
+        return cache.match(event.request).then(function (response) {
+          return response || fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
---
+++ slides/deck.md
@@ -0,0 +1,26 @@
+---
+marp: true
+theme: uncover
+_class: lead
+paginate: true
+backgroundColor: DarkSlateGrey
+---
+<!-- backgroundImage: "linear-gradient(to bottom, #483e51 , #1f1b23)" -->
+<!-- color: "#BFC9ED" -->
+# **Progressive Web Applications**
+
+## **.: quick introduction :.**
+by Daniel Drozdzewski :sweat_smile:
+**[@digi_noise]()**
+#
+##### 4th of December 2020 
+---
+# What is WPA?
+
+* Mobile :iphone:
+* Safe :lock:
+* Progressive :signal_strength:
+---
+#
+
+
